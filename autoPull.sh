01bbd9acb94936bc9b7658d24bf74570a80991a1
albertrein <guilhermealberto8@gmail.com>
2018-11-02 19:54:37 -0200

    Shell automatico para pull em varios diretorios.

---
+++ autoPull.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+
+diretorioRaiz=`pwd`
+
+for var in "$@" # Foreach de todos os argumentos passados por parametro 
+do
+	cd $var # Entra na pasta
+	if [ -d ".git" ]; then # É um repositório Git
+		echo "- Executando pull em "$var "$(tput sgr 0)"
+		git pull # Executa pull
+	fi
+	cd $diretorioRaiz # Volta a pasta raiz do projeto, usando path raiz para caso o git seja um sub-diretorio
+done;
